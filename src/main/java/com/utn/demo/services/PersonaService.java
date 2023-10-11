f61f1fc97b53a349969f988ff15fb000200719f1
valentinalencioni <143528811+valentinalencioni@users.noreply.github.com>
2023-10-11 00:36:20 -0300

    TP Paginacion

---
+++ src/main/java/com/utn/demo/services/PersonaService.java
@@ -0,0 +1,17 @@
+package com.utn.demo.services;
+
+import com.utn.demo.entities.Persona;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface PersonaService extends BaseService<Persona, Long> {
+    List<Persona> search(String filtro) throws Exception;
+
+    Page<Persona> search(String filtro, Pageable pageable) throws Exception;
+
+
+
+}
